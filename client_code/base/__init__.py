30d5211748a32503ed780c86e516f512470a7c56
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-05-18 11:52:37 +0000

    Edited base; Deleted Mycourses, courses and Home

--- client_code/base/__init__.py
+++ client_code/base/__init__.py
@@ -2,22 +2,16 @@ from ._anvil_designer import baseTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
-from ..Home import Home
 
 
 class base(baseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home())
+    self.label_1.text = "Hello world!"
 
-  def title_show(self, **event_args):
-    """This method is called when the Label is shown on the screen"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Home())
-
-    
+    # Any code you write here will run before the form opens.
