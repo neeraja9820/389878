b897d4aa8d7ac3ad15f85cc950a0c1c0531784e1
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-05-18 10:29:29 +0000

    Created Mycourses; Edited settings, base, courses and Home

--- client_code/Mycourses/__init__.py
+++ client_code/Mycourses/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import baseTemplate
+from ._anvil_designer import MycoursesTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -6,18 +6,11 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
-from ..Home import Home
 
 
-class base(baseTemplate):
+class Mycourses(MycoursesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home())
 
-  def title_show(self, **event_args):
-    """This method is called when the Label is shown on the screen"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Home())
-
-    
+    # Any code you write here will run before the form opens.
