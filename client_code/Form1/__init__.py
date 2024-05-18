378a296318593ada60543029dd77f0ead8cb7ed5
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-05-18 06:48:04 +0000

    Created Form1; Edited settings; Deleted first

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,13 +1,10 @@
-from ._anvil_designer import firstTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 
 
-class first(firstTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-  
-    
-  
 
     # Any code you write here will run before the form opens.
