5995de732a4b306e942f14b0c2c8b2368836923c
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-05-18 09:49:23 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import HomeTemplate
 from anvil import *
+from ..courses import courses
 
 
 class Home(HomeTemplate):
@@ -7,4 +8,8 @@ class Home(HomeTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(courses())
